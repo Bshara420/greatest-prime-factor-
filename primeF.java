6998015f88dcac2a9dc34d74cd90d1e3b66a43a0
Bshara420 <74296286+Bshara420@users.noreply.github.com>
2020-11-11 11:57:12 +0200

    Initial commit

---
+++ primeF.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.Scanner;
+
+public class test13 {
+
+	public static void main(String[] args) {
+		
+		int a, b, primeF=1;
+		try (Scanner user_input = new Scanner( System.in )) {
+			System.out.print("Enter Two Numbers ");
+			a  = user_input.nextInt( );
+			b = user_input.nextInt( );
+		}
+		
+		
+				for(int i=2; i<=a/2; i++) 
+				{
+					if(a%i==0 && b%i==0) 
+					{
+						 a=a/i;
+					     primeF=i;
+					}
+				}
+		if(primeF==1) 
+		{
+			System.out.println("there is no prime factor");
+			
+		}
+		else
+		{
+			System.out.println(primeF + " is the greatest prime factor");
+		}
+		
+	}
+	}
+	
